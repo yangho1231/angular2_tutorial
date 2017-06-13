29086873b4e6f1986aa992211432113aba0b8802
yangho1231 <yangho1231@gmail.com>
2017-06-12 20:45:58 -0600

    Added products folder

--- APM-Start-Updated/src/app/app.component.ts
+++ APM-Start-Updated/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'pm-app',
     template: `
-        <h1>Angular2: Getting Started</h1>
+        <pm-products></pm-products>
     `
 })
 export class AppComponent { }
